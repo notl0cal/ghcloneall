36cc3419e61a33be2bb8fd02d696927dae086767
chance-priv2 <forchancee@gmail.com>
2022-07-19 16:17:10 -0500

    fixed

--- ghcloneall.sh
+++ ghcloneall.sh
@@ -2,7 +2,6 @@
 USAGE="USAGE: ghcloneall <github username>"
 GHUSER=$1
 if ! [[ $1 ]]; then
-    echo $USAGE
-    exit 1
+    echo $USAGE; exit 1
 fi
 curl "https://api.github.com/users/$GHUSER/repos?per_page=1000" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git clone
